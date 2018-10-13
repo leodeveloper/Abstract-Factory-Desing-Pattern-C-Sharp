a5eb349062cce4ce08f56fe7aeaede0087be84bd
Muhammad Suleman <leodeveloper@gmail.com>
2018-10-13 12:12:59 +0100

    First Commit

--- AbstractFactoryDesignPattern/Program.cs
+++ AbstractFactoryDesignPattern/Program.cs
@@ -10,6 +10,21 @@ namespace AbstractFactoryDesignPattern
     {
         static void Main(string[] args)
         {
+            // Create and run the African animal world
+
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            // Create and run the American animal world
+
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+
+            // Wait for user input
+
+            Console.ReadKey();
         }
     }
 }
